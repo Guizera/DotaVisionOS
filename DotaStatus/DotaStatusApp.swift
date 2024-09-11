9a7a766b21a20c3c0a4165bc38fe3fee0f970b09
Jose Guilherme <joseg@octavebio.com>
2024-09-11 11:31:59 -0300

    Initial Commit

---
+++ DotaStatus/DotaStatusApp.swift
@@ -0,0 +1,17 @@
+//
+//  DotaStatusApp.swift
+//  DotaStatus
+//
+//  Created by José Alves da Cunha on 11/09/24.
+//
+
+import SwiftUI
+
+@main
+struct DotaStatusApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
